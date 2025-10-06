899e995d4a95b05bb080ffdd532a8d48960f2ee8
Raphaela Vidal <rapha.vidall@gmail.com>
2025-10-06 10:07:34 -0300

    mudanca de nome

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ScheduleEngine",
+    name: "SuggestionEngine",
     products: [
         .library(
             name: "SuggestionEngine",
