850b5e9a4c8df3379b691ca3443e2cfb599219c5
Raphaela Vidal <rapha.vidall@gmail.com>
2025-10-06 21:18:48 -0300

    feat(models): adicionar suporte a Int16 e String nos enums para compatibilidade com Core Data e JSON

--- Sources/SuggestionEngine/models/Intensity.swift
+++ Sources/SuggestionEngine/models/Intensity.swift
@@ -14,4 +14,26 @@ public enum Intensity: Int16, Comparable, CaseIterable, Sendable {
     public static func < (lhs: Intensity, rhs: Intensity) -> Bool {
         return lhs.rawValue < rhs.rawValue
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let intValue = try? container.decode(Int16.self) {
+            self = Intensity(rawValue: intValue) ?? .low
+        } else if let stringValue = try? container.decode(String.self) {
+            switch stringValue {
+            case "low": self = .low
+            case "medium": self = .medium
+            case "high": self = .high
+            default:
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Valor inválido: \(stringValue)")
+            }
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Tipo inválido para Goal")
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.rawValue)
+    }
 }
--- Sources/SuggestionEngine/models/Location.swift
+++ Sources/SuggestionEngine/models/Location.swift
@@ -11,4 +11,27 @@ import Foundation
 // Locais
 public enum Location: Int16, Codable, Sendable {
     case home = 1, gym = 2, work = 3, outdoor = 4
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let intValue = try? container.decode(Int16.self) {
+            self = Location(rawValue: intValue) ?? .home
+        } else if let stringValue = try? container.decode(String.self) {
+            switch stringValue {
+            case "home": self = .home
+            case "gym": self = .gym
+            case "work": self = .work
+            case "outdoor": self = .outdoor
+            default:
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Valor inválido: \(stringValue)")
+            }
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Tipo inválido para Location")
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.rawValue)
+    }
 }
